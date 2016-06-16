1931336f948782cbea6ba3a15e6b18881d265b65
Edward Crompton <edward.crompton@cameronandwilding.com>
2016-06-16 16:42:37 +0100

    Adding routes for the application and authenticating them all

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/tasks', 'TaskController@index');
+
+Route::post('/task', 'TaskController@store');
+
+Route::delete('/task/{task}', 'TaskController@destroy');
